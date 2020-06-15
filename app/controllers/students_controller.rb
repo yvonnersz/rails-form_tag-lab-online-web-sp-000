fe3c30415970fe49ae5559b329728c8d43e82faa
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-15 22:27:16 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,16 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def create
+    Student.create(params)
+    redirect_to students_path
   end
 
 end
